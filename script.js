71a6ead737fdcfe5a326267c4601646dfe8a13ec
Aswin <aswinkrishna363@gmail.com>
2023-01-30 22:01:11 +0530

    random array

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
+const cardAraay = [
+  {
+    name: 'cheeseburger',
+    image: 'cheeseburger.png'
+  },
 
+  {
+    name: 'fries',
+    image: 'fries.png'
+  },
+
+  {
+    name: 'hotdog',
+    image: 'hotdog.png'
+  },
+
+  {
+    name: 'ice-cream',
+    image: 'ice-cream.png'
+  },
+
+  {
+    name: 'milkshake',
+    image: 'milkshake.png'
+  },
+
+  {
+    name: 'pizza',
+    image: 'pizza.png'
+  },
+
+  {
+    name: 'cheeseburger',
+    image: 'cheeseburger.png'
+  },
+
+  {
+    name: 'fries',
+    image: 'fries.png'
+  },
+
+  {
+    name: 'hotdog',
+    image: 'hotdog.png'
+  },
+
+  {
+    name: 'ice-cream',
+    image: 'ice-cream.png'
+  },
+
+  {
+    name: 'milkshake',
+    image: 'milkshake.png'
+  },
+
+  {
+    name: 'pizza',
+    image: 'pizza.png'
+  }
+
+]
+
+// to get random
+cardAraay.sort( () => 0.5 - Math.random())
+console.log(cardAraay)
